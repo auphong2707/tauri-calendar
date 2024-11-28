a0361076ec1ed1134eb42faa03dd4a015e0b1c81
Veil <65228307+auphong2707@users.noreply.github.com>
2024-11-28 14:54:56 +0700

    Create the Calendar in Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Helmet } from 'react-helmet'
+import CssBaseLine from '@mui/material/CssBaseline'
+import BasicDateCalendar from './BasicDateCalendar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Helmet>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Helmet>
+      <CssBaseLine />
+
+      <div className='container'>
+        {/* Sidebar Section */}
+        <div className='sidebar'>
+          
+          {/* Top Section */}
+          <div className='sidebar-calendar'>
+            <div className='sidebar-calendar-header'>
+              <h2>Calendar</h2>
+            </div>
+            <BasicDateCalendar/>
+          </div>
+
+
+          {/* Bottom Section */}
+
+        </div>
+        {/* Main Section */}
+        <div className='main-content'>
+          Main Content Goes Here
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/BasicDateCalendar.jsx
@@ -0,0 +1,13 @@
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+
+export default function BasicDateCalendar() {
+  return (
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar/>
+      </LocalizationProvider>
+    </div>
+  );
+}
