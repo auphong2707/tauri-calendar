6216a528da4e64cf7043dd95e876a1ec1f2b0fbd
Veil <65228307+auphong2707@users.noreply.github.com>
2024-11-28 13:41:32 +0700

    Update Yarn

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install -y libcanberra-gtk-module libcanberra-gtk3-module
+sudo apt install -y libwebkit2gtk-4.1-dev build-essential curl wget file libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev
