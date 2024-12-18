0e761f595de8fde65d2cd03eba04d5fe9f6e1843
veil <auphong2707@gmail.com>
2024-12-18 21:57:52 +0700

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,18 @@
 // Import necessary modules
 import { FEATURE_CONSTANT } from '../constant';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 
 // Import Material UI components
 import { styled } from '@mui/material/styles';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 
 
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme }) => ({
     flexGrow: 1,
-    padding: theme.spacing(3),
+    height: `calc(100vh - ${FEATURE_CONSTANT.BAR_HEIGHT}px)`,
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
@@ -34,37 +36,104 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
   }),
 );
 
+const DayColumn = ({ day, index }) => {
+  const [dayName, date] = day.split('\n');
+
+  const DayColumnHeader = () => (
+    <Box 
+      sx={{ 
+        borderLeft: (index !== 0 ? 1 : 0), 
+        borderColor: 'white', 
+        height: '10%', 
+        textAlign: 'center',
+        alignContent: 'center',
+        backgroundColor: 'olive',
+        zIndex: 0,
+      }}
+    >
+      <Typography variant="h6" color='white'>{dayName}</Typography>
+      <Typography variant="body2" color='white'>{date}</Typography>
+    </Box>
+  );
+
+  const rows = Array.from({ length: 12 });
+  const DayColumnBackground = () => (
+    <Box
+      sx={{
+        borderLeft: (index !== 0 ? 1 : 0),
+        borderColor: 'white',
+        height: '90%',
+        width: '100%',
+        textAlign: 'center',
+        alignContent: 'center',
+        backgroundColor: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        zIndex: 0,
+        position: 'absolute',
+      }}
+    >
+      {/* Rows with lines */}
+      {rows.map((_, rowIndex) => (
+        <Box
+          key={rowIndex}
+          sx={{
+            flexGrow: 1,
+            backgroundColor: '#F5F5DC',
+            borderBottom: 1,
+            borderTop: (rowIndex === 0 ? 1 : 0),
+            borderColor: 'white',
+          }}
+        >
+        </Box>
+      ))}
+    </Box>
+  );
+
+  const DayColumnContent = () => (
+    <Box
+      sx={{
+        borderLeft: (index !== 0 ? 1 : 0),
+        borderColor: 'white',
+        height: '90%',
+        textAlign: 'center',
+        alignContent: 'center',
+        backgroundColor: 'transparent',
+        display: 'flex',
+        flexDirection: 'column',
+        zIndex: 1,
+        position: 'relative',
+      }}
+    >
+      <Typography variant="h6" color='black'>Content</Typography>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ height: '100%', width: '100%', position: 'relative' }}>
+      <DayColumnHeader />
+      <DayColumnBackground />
+      <DayColumnContent />
+    </Box>
+  );
+};
+
+DayColumn.propTypes = {
+  day: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
 export default function MainContent({ isSidebarOpen }) {
   return (
-    <Main open={isSidebarOpen}>
-        <Typography sx={{ marginBottom: 2 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography sx={{ marginBottom: 2 }}>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Main>
+    <Main open={isSidebarOpen} >
+      <Grid container columns={7} sx={{ height: '100%' }}>
+        {Array.from({ length: 7 }).map((_, index) => (
+          <Grid key={index} size={1} item sx={{ height: '100%' }}>
+            <DayColumn day={dayjs().add(index, 'day').format("dddd\nDD/MM/YYYY").toString()} index={index} />
+          </Grid>
+        ))}
+      </Grid>
+    </Main>
   );
 }
 
